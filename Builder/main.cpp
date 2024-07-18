c0cd5f8cb0d3d14006a850d92164c48b6cb3990f
Margaret Alade <alade.margaret@yandex.ru>
2024-07-18 08:24:49 +0300

    Initial commit

--- Builder/main.cpp
+++ Builder/main.cpp
@@ -6,9 +6,102 @@
 //
 
 #include <iostream>
+#include <pqxx/pqxx>
+
+
+struct Studentdata {
+    std::string table_name = "";
+    std::string column_name = "";
+    std::map<std::string, std::string> column_where;
+};
+
+
+class SqlSelectQueryBuilder {
+public:
+    SqlSelectQueryBuilder() = default;
+    
+    SqlSelectQueryBuilder& AddColumn(std::string column) {
+        if (studentdata.column_name != "") {
+            studentdata.column_name += ", " + column;
+        } else {
+            studentdata.column_name = column;
+        }
+        return *this;
+    }
+    
+    SqlSelectQueryBuilder& AddFrom(std::string table) {
+        studentdata.table_name = table;
+        return *this;
+    }
+    
+    SqlSelectQueryBuilder& AddWhere(std::string key, std::string value) {
+        studentdata.column_where[key] = value;
+        return *this;
+    }
+    
+    void BuildQuery(pqxx::connection& conn) {
+        pqxx::work work1(conn);
+        auto result_rows = work1.query<int, std::string, std::string, std::string>("SELECT " + studentdata.column_name + " FROM " + studentdata.table_name);
+        for (auto & result : result_rows) {
+            std::cout << get<0>(result) << ", " << get<1>(result) << ", " << get<2>(result) << ", " << get<3>(result) << "\n";
+        }
+        
+        work1.commit();
+    }
+    
+private:
+    Studentdata studentdata;
+};
+
+void createTable(pqxx::connection& conn) {
+    pqxx::work work1(conn);
+    work1.exec("CREATE TABLE IF NOT EXISTS Students (id serial PRIMARY KEY, name varchar(20) NOT NULL, surname varchar(20) NOT NULL, number varchar(20) NOT NULL);");
+    work1.commit();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    try {
+        pqxx::connection conn (
+                              "host=MacBook-Air-Margaret.local "
+                              "port=5432 "
+                              "dbname=builder "
+                              "user=postgres "
+                              "password=8563"
+                              );
+        
+        std::cout << "Connection is established" << "\n";
+        
+        createTable(conn);
+        
+        pqxx::work work1(conn);
+        work1.exec("INSERT INTO Students(name, surname, number) "
+                   "VALUES('Николай', 'Орлов', '+79058035621')");
+        work1.commit();
+        
+        pqxx::work work2(conn);
+        work2.exec("INSERT INTO Students(name, surname, number) "
+                   "VALUES('Cветлана', 'Китаева', '+79789032233')");
+        work2.commit();
+        
+        pqxx::work work3(conn);
+        work3.exec("INSERT INTO Students(name, surname, number) "
+                   "VALUES('Вячеслав', 'Тимофеев', '+79917436741')");
+        work3.commit();
+        
+        SqlSelectQueryBuilder query;
+        query.AddColumn("id")
+             .AddColumn("name")
+             .AddColumn("surname")
+             .AddColumn("number")
+             .AddFrom("Students");
+        query.BuildQuery(conn);
+        
+        
+        return 0;
+        
+    } catch (std::exception& e) {
+        std::cout << "Exception: " << e.what() << "\n";
+    }
     return 0;
 }
