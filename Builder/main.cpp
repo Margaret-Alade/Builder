7be635d3bd307db06a1be89f331793c817a81b63
Margaret Alade <alade.margaret@yandex.ru>
2024-07-25 07:19:54 +0300

    Correct Homework

--- Builder/main.cpp
+++ Builder/main.cpp
@@ -11,8 +11,9 @@
 
 struct Studentdata {
     std::string table_name = "";
-    std::string column_name = "";
-    std::map<std::string, std::string> column_where;
+    std::string column_name = "*";
+    std::map<std::string, std::string> column_where_data;
+    std::string column_where = "";
 };
 
 
@@ -21,7 +22,7 @@ public:
     SqlSelectQueryBuilder() = default;
     
     SqlSelectQueryBuilder& AddColumn(std::string column) {
-        if (studentdata.column_name != "") {
+        if (studentdata.column_name != "*") {
             studentdata.column_name += ", " + column;
         } else {
             studentdata.column_name = column;
@@ -34,16 +35,45 @@ public:
         return *this;
     }
     
-    SqlSelectQueryBuilder& AddWhere(std::string key, std::string value) {
-        studentdata.column_where[key] = value;
+    SqlSelectQueryBuilder& AddWhereOr(std::string key, std::string value) {
+        
+        if (!studentdata.column_where_data.empty()) {
+            studentdata.column_where_data[key] = value;
+            studentdata.column_where += " OR " +  key + "=" + value;
+        } else {
+            studentdata.column_where_data[key] = value;
+            studentdata.column_where = key + "=" + value;
+        }
+        return *this;
+    }
+    
+    SqlSelectQueryBuilder& AddWhereAnd(std::string key, std::string value) {
+        
+        if (!studentdata.column_where_data.empty()) {
+            studentdata.column_where_data[key] = value;
+            studentdata.column_where += " AND " +  key + "=" + value;
+        } else {
+            studentdata.column_where_data[key] = value;
+            studentdata.column_where = key + "=" + value;
+        }
         return *this;
     }
     
     void BuildQuery(pqxx::connection& conn) {
         pqxx::work work1(conn);
-        auto result_rows = work1.query<int, std::string, std::string, std::string>("SELECT " + studentdata.column_name + " FROM " + studentdata.table_name);
-        for (auto & result : result_rows) {
-            std::cout << get<0>(result) << ", " << get<1>(result) << ", " << get<2>(result) << ", " << get<3>(result) << "\n";
+        
+        if (studentdata.column_where_data.empty()) {
+            auto result_rows = work1.query<int, std::string, std::string, std::string>("SELECT " + studentdata.column_name + " FROM " + studentdata.table_name );
+            for (auto & result : result_rows) {
+                std::cout << get<0>(result) << ", " << get<1>(result) << ", " << get<2>(result) << ", " << get<3>(result) << "\n";
+            }
+            
+        } else {
+            auto result_rows = work1.query<int, std::string, std::string, std::string>("SELECT " + studentdata.column_name + " FROM " + studentdata.table_name + " WHERE " + studentdata.column_where);
+            
+            for (auto & result : result_rows) {
+                std::cout << get<0>(result) << ", " << get<1>(result) << ", " << get<2>(result) << ", " << get<3>(result) << "\n";
+            }
         }
         
         work1.commit();
@@ -72,31 +102,33 @@ int main(int argc, const char * argv[]) {
         
         std::cout << "Connection is established" << "\n";
         
-        createTable(conn);
-        
-        pqxx::work work1(conn);
-        work1.exec("INSERT INTO Students(name, surname, number) "
-                   "VALUES('Николай', 'Орлов', '+79058035621')");
-        work1.commit();
-        
-        pqxx::work work2(conn);
-        work2.exec("INSERT INTO Students(name, surname, number) "
-                   "VALUES('Cветлана', 'Китаева', '+79789032233')");
-        work2.commit();
-        
-        pqxx::work work3(conn);
-        work3.exec("INSERT INTO Students(name, surname, number) "
-                   "VALUES('Вячеслав', 'Тимофеев', '+79917436741')");
-        work3.commit();
+//        createTable(conn);
+//        
+//        pqxx::work work1(conn);
+//        work1.exec("INSERT INTO Students(name, surname, number) "
+//                   "VALUES('Николай', 'Орлов', '+79058035621')");
+//        work1.commit();
+//        
+//        pqxx::work work2(conn);
+//        work2.exec("INSERT INTO Students(name, surname, number) "
+//                   "VALUES('Cветлана', 'Китаева', '+79789032233')");
+//        work2.commit();
+//        
+//        pqxx::work work3(conn);
+//        work3.exec("INSERT INTO Students(name, surname, number) "
+//                   "VALUES('Вячеслав', 'Тимофеев', '+79917436741')");
+//        work3.commit();
         
         SqlSelectQueryBuilder query;
+        
         query.AddColumn("id")
              .AddColumn("name")
              .AddColumn("surname")
              .AddColumn("number")
-             .AddFrom("Students");
+             .AddFrom("Students")
+             .AddWhereOr("name", "'Николай'")
+             .AddWhereOr("surname", "'Китаева'");
         query.BuildQuery(conn);
-        
         
         return 0;
         
